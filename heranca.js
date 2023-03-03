22d707f9288d3e7f50232844dced4b1011d435a0
retr0 <retr0lbb@gmail.com>
2023-03-03 01:39:27 -0300

    add xml file

--- heranca.js
+++ heranca.js
@@ -1,8 +1,9 @@
 class Animals{
-    constructor(name,age,specie){
+    constructor(name,age,specie,lifeTime){
         this.name = name
         this.age = age
         this.specie = specie
+        this.lifeTime = lifeTime
     }
     eat(){
 
@@ -15,31 +16,48 @@ class Animals{
     }
 }
 class Mammals extends Animals{
-    constructor(name,age,specie,fur,gestation){
-        super(name,age,specie)
-        this.fur = fur
-        this.gestation = gestation
+    constructor(name,age,specie,lifeTime,move){
+        super(name,age,specie,lifeTime)
+        this.move = move
+    }
+    hibern(){
+
     }
 
 }
 class Birds extends Animals{
-    constructor(name,age,specie,fly,egg){
-        super(name,age,specie)
+    constructor(name,age,specie,lifeTime,fly,egg){
+        super(name,age,specie,lifeTime)
         this.fly = fly
         this.egg = egg
     }
-}
+    hunt(){
 
-class Dog extends( Animals, Mammals){
-    constructor(name, age, specie, fur, gestation, agressive, size){
-        super(name, age, specie, fur,gestation)
-        this.agressive = agressive
-        this.size = size
     }
+}
+class Fish extends Animals{
+    constructor(name,age,specie,lifeTime,bones){
+        super(name,age,specie,lifeTime)
+        this.bones = bones
+    }
+    swim(){
 
+    }
 }
 
+class Reptile extends Animals{
+    constructor(name,age,specie,lifeTime,venon){
+        super(name,age,specie,lifeTime)
+        this.venon = venon
+    }
+    regenerate(){
 
-const ruffles = new Dog('ruffles', 1, 'bulldog', true, 9, false, 'Medium')
+    }
+}
 
-console.log(ruffles)
+const ruffles = new Mammals('ruffles',3,'pug',15,'run')
+const mordecai = new Birds('Mordecai', 1, 'mordecai', 5, true, true)
+const darwin = new Fish('Darwin', 2, 'goldenFish',10, false)
+const snake = new Reptile('croba', 5, 'cascavel', 15, true)
+
+console.log(ruffles, mordecai, darwin, snake)
